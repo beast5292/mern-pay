d7e62e340492cb37fee95020e547fc28029d69be
Mindula Perera <145087911+beast5292@users.noreply.github.com>
2025-07-22 23:49:18 +0530

    Project setup completed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='form'>
+      MernPay
+    </div>
+  )
+}
+
+export default App
